02b991da24f515b57ba2eeb62ae77f99865a19ac
Selim Elsayed <saleemelsayed17@gmail.com>
2023-05-14 08:44:18 +0300

    selim

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,26 +1,53 @@
 #!/usr/bin/python3
-"""
-Test suits for amenities
-"""
+"""Unittest module for the City Class."""
 
-import os
-import models
 import unittest
 from datetime import datetime
+import time
+from models.city import City
+import re
+import json
+from models.engine.file_storage import FileStorage
+import os
+from models import storage
 from models.base_model import BaseModel
 
 
 class TestCity(unittest.TestCase):
-    """
-    Tests for amenities
-    """
 
-    def test_name(self):
-        """
-        Tests for name inputs
-        """
+    """Test Cases for the City class."""
+
+    def setUp(self):
+        """Sets up test methods."""
         pass
 
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
 
-if __name__ == '__main__':
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_8_instantiation(self):
+        """Tests instantiation of City class."""
+
+        b = City()
+        self.assertEqual(str(type(b)), "<class 'models.city.City'>")
+        self.assertIsInstance(b, City)
+        self.assertTrue(issubclass(type(b), BaseModel))
+
+    def test_8_attributes(self):
+        """Tests the attributes of City class."""
+        attributes = storage.attributes()["City"]
+        o = City()
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(o, k))
+            self.assertEqual(type(getattr(o, k, None)), v)
+
+
+if __name__ == "__main__":
     unittest.main()
