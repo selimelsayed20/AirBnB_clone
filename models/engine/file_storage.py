a34147306ac8d9d1a09cebbc9b53a0fce7e078a4
Selim Elsayed <saleemelsayed17@gmail.com>
2023-05-14 08:27:48 +0300

    selim

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,78 +1,57 @@
 #!/usr/bin/python3
-"""Defines a class FileStorage.
-"""
 import json
 import os
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+"""serializes instances to a JSON file\
+        and deserializes JSON file to instances"""
 
 
-class FileStorage():
-    """Class that serializes instances to a JSON file and deserializes
-    JSON file to instances.
-    """
+class FileStorage:
+    """Implementation"""
     __file_path = "file.json"
     __objects = {}
 
-    def __init__(self):
-        """Creates new instances of class.
-        """
-        pass
-
     def all(self):
-        """Returns the dictionary objects.
-
-        Returns:
-            dict: objects.
-        """
+        """Returns the dictionary"""
         return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id.
-
-        Args:
-            obj (any): object.
-        """
-        key = obj.__class__.__name__ + "." + obj.id
-        self.__objects[key] = obj
+        """sets in the __objects the obj with key <obj classname>.id"""
+        # serializing in this method will make object to be
+        # saved without instances
+        # suppose u serialize here, you must save
+        # immediately inorder to store in the disk
+        # FileStorage.__objects[obj.__class__.__name__
+        # + '.' + obj.id] = obj.to_dict()
+        # instead, store the objects in the
+        # FileStorage.__objects and serialize in the save() method
+        self.__objects[obj.__class__.__name__ + '.' + obj.id] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path).
-        """
+        """serializes __objects to the JSON file"""
         dictionary = {}
-
-        for key, value in FileStorage.__objects.items():
-            dictionary[key] = value.to_dict()
-
-        with open(FileStorage.__file_path, 'w', encoding="utf-8") as myFile:
-            json.dump(dictionary, myFile)
+        for key, values in FileStorage.__objects.items():
+            dictionary[key] = values.to_dict()
+        with open(FileStorage.__file_path, 'w') as output:
+             json.dump(dictionary, output)
+                # This will not work well. Object instances wont be saved
+                # with open(self.__file_path, 'w') as output:
+                # json.dump(FileStorage.__objects, output)
 
     def reload(self):
-        """Deserializes the JSON file to __objects only if the JSON file
-        (__file_path) exists ; otherwise, do nothing. If the file doesn’t
-        exist, no exception should be raised)
-        """
-        try:
-            with open(self.__file_path, 'r', encoding='utf-8') as myFile:
-                my_obj_dump = myFile.read()
-        except Exception:
-            return
-        objects = eval(my_obj_dump)
-        for (key, value) in objects.items():
-            objects[key] = eval(key.split('.')[0] + '(**value)')
-        self.__objects = objects
-
-    def delete(self, obj):
-        """Deletes obj from __objects
-        """
-        try:
-            key = obj.__class__.__name__ + '.' + str(obj.id)
-            del self.__objects[key]
-            return True
-        except Exception:
-            return False
+        """deserializes the JSON file to __objects only if JSON file\
+                exists; otherwise do nothing. If the file doesn't exist\
+                no exception should be raised"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.city import City
+        from models.amenity import Amenity
+        from models.state import State
+        from models.review import Review
+        dct = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
+               'City': City, 'Amenity': Amenity, 'State': State,
+               'Review': Review}
+        if os.path.exists(self.__file_path) is True:
+            with open(self.__file_path, 'r') as f:
+                for key, value in json.load(f).items():
+                    self.new(dct[value['__class__']](**value))
